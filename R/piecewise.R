64d3f171b7135d25223e4edf858fc637ad762f13
Harrison Reeder <reeder.h@gmail.com>
2020-06-24 09:59:12 -0400

    add piecewise and rp helper functions

---
+++ R/piecewise.R
@@ -0,0 +1,395 @@
+nlogLikPW_ID_frail_SM <- function(para, y1, y2, delta1, delta2,
+                                  Xmat1, Xmat2, Xmat3,
+                                  basis1, basis2, basis3){
+  # browser()
+
+  #count of the number of intervals in each list
+  num_int <- c(ncol(basis1),ncol(basis2),ncol(basis3))
+  n <- length(y1)
+
+  phi1 <- para[1:num_int[1]]
+  phi2 <- para[(1+num_int[1]):(num_int[1]+num_int[2])]
+  phi3 <- para[(1+num_int[1]+num_int[2]):(num_int[1]+num_int[2]+num_int[3])]
+  theta <- exp(para[(1+num_int[1]+num_int[2]+num_int[3]):(num_int[1]+num_int[2]+num_int[3]+1)])
+
+  if(!is.null(Xmat1) && !(ncol(Xmat1)==0)){
+    nP1 <- ncol(Xmat1)
+    beta1 <- para[(num_int[1]+num_int[2]+num_int[3]+1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1)]
+    eta1 <- as.vector(Xmat1 %*% beta1)
+  } else{
+    nP1 <- 0
+    eta1 <- 0
+  }
+  if(!is.null(Xmat2) && !(ncol(Xmat2)==0)){
+    nP2 <- ncol(Xmat2)
+    beta2 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2)]
+    eta2 <- as.vector(Xmat2 %*% beta2)
+  } else{
+    nP2 <- 0
+    eta2 <- 0
+  }
+  if(!is.null(Xmat3) && !(ncol(Xmat3)==0)){
+    nP3 <- ncol(Xmat3)
+    beta3 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+nP2+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2+nP3)]
+    eta3 <- as.vector(Xmat3 %*% beta3)
+  } else{
+    nP3 <- 0
+    eta3 <- 0
+  }
+
+  #matrix with each row being an observation, and each column being an interval, capturing how much time is accrued in each interval
+  # knots1_mat <- matrix(c(knots1_diff,0),nrow=n,ncol=num_int[1],byrow = TRUE)
+  # knots2_mat <- matrix(c(knots2_diff,0),nrow=n,ncol=num_int[2],byrow = TRUE)
+
+  #an n length vector of integers indicating which interval the observation is in
+  cut_cats1 <- rowSums(basis1!=0)
+  cut_cats2 <- rowSums(basis2!=0)
+  cut_cats3 <- rowSums(basis3!=0) #ASSUMES SEMIMARKOV FOR THE MOMENT
+  cut_cats3[cut_cats3==0] <- 1 #for bookkeeping, put people who never accrue time into interval 1
+
+  #computes n length vector of the hazard at the failure time of each observation
+  haz01 <- exp(phi1)[cut_cats1]
+  haz02 <- exp(phi2)[cut_cats2]
+  haz03 <- exp(phi3)[cut_cats3]
+
+  #fill in the spots that accrue no time in the third hazard with NAs
+  # haz03 <- rep(1,n) #placeholder, never gets used
+  # haz03[delta1==1] <- exp(phi3)[cut_cats3]
+
+  #computes n length vector of the cumulative hazard at the failure time of each observation
+  Lambda01 <- basis1 %*% exp(phi1)
+  Lambda02 <- basis2 %*% exp(phi2)
+  Lambda03 <- basis3 %*% exp(phi3)
+
+  # browser()
+
+  ll <- sum(
+    delta1 * (log(haz01) + eta1) +
+      (1-delta1)*delta2*(log(haz02) + eta2) +
+      delta1*delta2*(log(haz03) + eta3 + log(1 + theta)) -
+      (theta^(-1) + delta1 + delta2)* as.vector(log(1 + theta*(Lambda01*exp(eta1) +
+                                                                 Lambda02*exp(eta2) +
+                                                                 Lambda03*exp(eta3)
+      )
+      )
+      )
+  )
+  return(-ll)
+}
+
+
+nlogLikPW_ID_frail_M <- function(para, y1, y2, delta1, delta2,
+                                 Xmat1 = NULL, Xmat2 = NULL, Xmat3 = NULL,
+                                 basis1, basis2, basis3, basis3_y1){
+  # browser()
+
+  #count of the number of intervals in each list
+  num_int <- c(ncol(basis1),ncol(basis2),ncol(basis3))
+  n <- length(y1)
+
+  phi1 <- para[1:num_int[1]]
+  phi2 <- para[(1+num_int[1]):(num_int[1]+num_int[2])]
+  phi3 <- para[(1+num_int[1]+num_int[2]):(num_int[1]+num_int[2]+num_int[3])]
+  theta <- exp(para[(1+num_int[1]+num_int[2]+num_int[3]):(num_int[1]+num_int[2]+num_int[3]+1)])
+
+  if(!is.null(Xmat1) && !(ncol(Xmat1)==0)){
+    nP1 <- ncol(Xmat1)
+    beta1 <- para[(num_int[1]+num_int[2]+num_int[3]+1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1)]
+    eta1 <- as.vector(Xmat1 %*% beta1)
+  } else{
+    nP1 <- 0
+    eta1 <- 0
+  }
+  if(!is.null(Xmat2) && !(ncol(Xmat2)==0)){
+    nP2 <- ncol(Xmat2)
+    beta2 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2)]
+    eta2 <- as.vector(Xmat2 %*% beta2)
+  } else{
+    nP2 <- 0
+    eta2 <- 0
+  }
+  if(!is.null(Xmat3) && !(ncol(Xmat3)==0)){
+    nP3 <- ncol(Xmat3)
+    beta3 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+nP2+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2+nP3)]
+    eta3 <- as.vector(Xmat3 %*% beta3)
+  } else{
+    nP3 <- 0
+    eta3 <- 0
+  }
+
+  #matrix with each row being an observation, and each column being an interval, capturing how much time is accrued in each interval
+  # knots1_mat <- matrix(c(knots1_diff,0),nrow=n,ncol=num_int[1],byrow = TRUE)
+  # knots2_mat <- matrix(c(knots2_diff,0),nrow=n,ncol=num_int[2],byrow = TRUE)
+
+  #an n length vector of integers indicating which interval the observation is in
+  cut_cats1 <- rowSums(basis1!=0)
+  cut_cats2 <- rowSums(basis2!=0)
+  cut_cats3 <- rowSums(basis3!=0) #ASSUMES MARKOV FOR THE MOMENT
+
+  #computes n length vector of the hazard at the failure time of each observation
+  haz01 <- exp(phi1)[cut_cats1]
+  haz02 <- exp(phi2)[cut_cats2]
+  haz03 <- exp(phi3)[cut_cats3]
+
+  #computes n length vector of the cumulative hazard at the failure time of each observation
+  Lambda01 <- basis1 %*% exp(phi1)
+  Lambda02 <- basis2 %*% exp(phi2)
+  Lambda03 <- (basis3 - basis3_y1) %*% exp(phi3)
+
+  #browser()
+  ll <- sum(
+    delta1 * (log(haz01) + eta1) +
+      (1-delta1)*delta2*(log(haz02) + eta2) +
+      delta1*delta2*(log(haz03) + eta3 + log(1 + theta)) -
+      (theta^(-1) + delta1 + delta2)* as.vector(log(1 + theta*(Lambda01*exp(eta1) +
+                                                                 Lambda02*exp(eta2) +
+                                                                 Lambda03*exp(eta3)
+      )
+      )
+      )
+  )
+  return(-ll)
+}
+
+
+ngradPW_ID_frail_SM <- function(para, y1, y2, delta1, delta2,
+                                Xmat1 = NULL, Xmat2 = NULL, Xmat3 = NULL,
+                                basis1, basis2, basis3){
+  # browser()
+
+  #count of the number of intervals in each list
+  num_int <- c(ncol(basis1),ncol(basis2),ncol(basis3))
+  n <- length(y1)
+
+  phi1 <- para[1:num_int[1]]
+  phi2 <- para[(1+num_int[1]):(num_int[1]+num_int[2])]
+  phi3 <- para[(1+num_int[1]+num_int[2]):(num_int[1]+num_int[2]+num_int[3])]
+  theta <- exp(para[(1+num_int[1]+num_int[2]+num_int[3]):(num_int[1]+num_int[2]+num_int[3]+1)])
+  h <- log(theta)
+
+  if(!is.null(Xmat1) && !(ncol(Xmat1)==0)){
+    nP1 <- ncol(Xmat1)
+    beta1 <- para[(num_int[1]+num_int[2]+num_int[3]+1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1)]
+    eta1 <- as.vector(Xmat1 %*% beta1)
+  } else{
+    nP1 <- 0
+    eta1 <- 0
+  }
+  if(!is.null(Xmat2) && !(ncol(Xmat2)==0)){
+    nP2 <- ncol(Xmat2)
+    beta2 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2)]
+    eta2 <- as.vector(Xmat2 %*% beta2)
+  } else{
+    nP2 <- 0
+    eta2 <- 0
+  }
+  if(!is.null(Xmat3) && !(ncol(Xmat3)==0)){
+    nP3 <- ncol(Xmat3)
+    beta3 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+nP2+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2+nP3)]
+    eta3 <- as.vector(Xmat3 %*% beta3)
+  } else{
+    nP3 <- 0
+    eta3 <- 0
+  }
+
+  #matrix with each row being an observation, and each column being an interval, capturing how much time is accrued in each interval
+  # knots1_mat <- matrix(c(knots1_diff,0),nrow=n,ncol=num_int[1],byrow = TRUE)
+  # knots2_mat <- matrix(c(knots2_diff,0),nrow=n,ncol=num_int[2],byrow = TRUE)
+
+  #an n length vector of integers indicating which interval the observation is in
+  cut_cats1 <- rowSums(basis1!=0)
+  cut_cats2 <- rowSums(basis2!=0)
+  cut_cats3 <- rowSums(basis3!=0) #ASSUMES SEMIMARKOV FOR THE MOMENT
+  cut_cats3[cut_cats3==0] <- 1
+
+  #computes n length vector of the cumulative hazard at the failure time of each observation
+  Lambda01 <- basis1 %*% exp(phi1)
+  Lambda02 <- basis2 %*% exp(phi2)
+  Lambda03 <- basis3 %*% exp(phi3)
+
+  #code to multiply each row of Lambda0g elementwise by the vector exp(phig)
+  #this gives us exactly what we want for Lambda0gPhi (faster version of 'sweep')
+  #https://stackoverflow.com/questions/49462591/r-multiply-every-row-of-df-or-matrix-with-a-vector/49462658
+
+  Lambda01Phi <- basis1*t(exp(phi1))[rep_len(1,nrow(Lambda01)),]
+  Lambda02Phi <- basis2*t(exp(phi2))[rep_len(1,nrow(Lambda02)),]
+  Lambda03Phi <- basis3*t(exp(phi3))[rep_len(1,nrow(Lambda03)),]
+
+  #browser()
+
+  AVec <- Lambda01 * exp(eta1) + Lambda02 * exp(eta2) + Lambda03 * exp(eta3)
+  commonVec <- (exp(-h) + delta1 + delta2) / (1+exp(h)*AVec)
+
+  #first term is just counting how many people experience the nonterminal event in each interval
+  temp_val <- tapply(delta1,cut_cats1,sum)
+  if(length(temp_val) != num_int[1]){stop("It looks like you do not observe events in every interval of the first transition")}
+  score_phi1 <- temp_val - as.vector( t(Lambda01Phi) %*% (commonVec * exp(h + eta1)) )
+
+  #first term is just counting how many people experience the terminal event without the nonterminal in each interval
+  temp_val <- tapply((1-delta1)*delta2,cut_cats2,sum)
+  if(length(temp_val) != num_int[2]){stop("It looks like you do not observe events in every interval of the second transition")}
+  score_phi2 <- temp_val - as.vector( t(Lambda02Phi) %*% (commonVec * exp(h + eta2)) )
+
+  #first term is just counting how many people experience the terminal event after the nonterminal in each interval
+  temp_val <- tapply((delta1)*delta2,cut_cats3,sum)
+  if(length(temp_val) != num_int[3]){stop("It looks like you do not observe events in every interval of the third transition")}
+  score_phi3 <- temp_val - as.vector(  t(Lambda03Phi) %*% (commonVec * exp(h + eta3)) )
+
+  #h (what ina calls u1)
+  score_h <- sum(exp(h)*(delta1 * delta2/(1+exp(h)) + log(1+exp(h) * AVec)/exp(2*h) - commonVec * AVec))
+
+  #beta1 (what ina calls u2)
+  if(nP1 == 0){ score_beta1 <- NULL } else{
+    score_beta1 <-  t(Xmat1) %*% (delta1 - commonVec * Lambda01 * exp(h + eta1))
+  }
+
+  #beta2 (what ina calls u3)
+  if(nP2 == 0){ score_beta2 <- NULL } else{
+    score_beta2 <- t(Xmat2) %*% ( (1-delta1) * delta2 - commonVec * Lambda02 * exp(h + eta2))
+  }
+
+  #beta3 (what ina calls u4)
+  if(nP3 == 0){ score_beta3 <- NULL } else{
+    score_beta3 <-  t(Xmat3) %*% (delta1 * delta2 - commonVec * Lambda03 * exp(h + eta3))
+  }
+
+  return(-c(score_phi1,score_phi2,score_phi3,score_h,score_beta1,score_beta2,score_beta3))
+
+}
+
+
+
+
+ngradPW_ID_frail_M <- function(para, y1, y2, delta1, delta2,
+                               Xmat1 = NULL, Xmat2 = NULL, Xmat3 = NULL,
+                               basis1, basis2, basis3, basis3_y1){
+  # browser()
+
+  #count of the number of intervals in each list
+  num_int <- c(ncol(basis1),ncol(basis2),ncol(basis3))
+  n <- length(y1)
+
+  phi1 <- para[1:num_int[1]]
+  phi2 <- para[(1+num_int[1]):(num_int[1]+num_int[2])]
+  phi3 <- para[(1+num_int[1]+num_int[2]):(num_int[1]+num_int[2]+num_int[3])]
+  theta <- exp(para[(1+num_int[1]+num_int[2]+num_int[3]):(num_int[1]+num_int[2]+num_int[3]+1)])
+  h <- log(theta)
+
+  if(!is.null(Xmat1) && !(ncol(Xmat1)==0)){
+    nP1 <- ncol(Xmat1)
+    beta1 <- para[(num_int[1]+num_int[2]+num_int[3]+1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1)]
+    eta1 <- as.vector(Xmat1 %*% beta1)
+  } else{
+    nP1 <- 0
+    eta1 <- 0
+  }
+  if(!is.null(Xmat2) && !(ncol(Xmat2)==0)){
+    nP2 <- ncol(Xmat2)
+    beta2 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2)]
+    eta2 <- as.vector(Xmat2 %*% beta2)
+  } else{
+    nP2 <- 0
+    eta2 <- 0
+  }
+  if(!is.null(Xmat3) && !(ncol(Xmat3)==0)){
+    nP3 <- ncol(Xmat3)
+    beta3 <- para[(1+num_int[1]+num_int[2]+num_int[3]+nP1+nP2+1):(num_int[1]+num_int[2]+num_int[3]+1+nP1+nP2+nP3)]
+    eta3 <- as.vector(Xmat3 %*% beta3)
+  } else{
+    nP3 <- 0
+    eta3 <- 0
+  }
+
+  #matrix with each row being an observation, and each column being an interval, capturing how much time is accrued in each interval
+  # knots1_mat <- matrix(c(knots1_diff,0),nrow=n,ncol=num_int[1],byrow = TRUE)
+  # knots2_mat <- matrix(c(knots2_diff,0),nrow=n,ncol=num_int[2],byrow = TRUE)
+
+  #an n length vector of integers indicating which interval the observation is in
+  cut_cats1 <- rowSums(basis1!=0)
+  cut_cats2 <- rowSums(basis2!=0)
+  cut_cats3 <- rowSums(basis3!=0) #ASSUMES MARKOV FOR THE MOMENT
+
+  #computes n length vector of the cumulative hazard at the failure time of each observation
+  Lambda01 <- basis1 %*% exp(phi1)
+  Lambda02 <- basis2 %*% exp(phi2)
+  Lambda03 <- (basis3 - basis3_y1) %*% exp(phi3)
+
+  #code to multiply each row of Lambda0g elementwise by the vector exp(phig)
+  #this gives us exactly what we want for Lambda0gPhi (faster version of 'sweep')
+  #https://stackoverflow.com/questions/49462591/r-multiply-every-row-of-df-or-matrix-with-a-vector/49462658
+
+  Lambda01Phi <- basis1*t(exp(phi1))[rep_len(1,nrow(Lambda01)),]
+  Lambda02Phi <- basis2*t(exp(phi2))[rep_len(1,nrow(Lambda02)),]
+  Lambda03Phi <- (basis3 - basis3_y1)*t(exp(phi3))[rep_len(1,nrow(Lambda03)),]
+
+  #browser()
+
+  AVec <- Lambda01 * exp(eta1) + Lambda02 * exp(eta2) + Lambda03 * exp(eta3)
+  commonVec <- (exp(-h) + delta1 + delta2) / (1+exp(h)*AVec)
+
+  #first term is just counting how many people experience the nonterminal event in each interval
+  temp_val <- tapply(delta1,cut_cats1,sum)
+  if(length(temp_val) != num_int[1]){stop("It looks like you do not observe events in every interval of the first transition")}
+  score_phi1 <- temp_val - as.vector( t(Lambda01Phi) %*% (commonVec * exp(h + eta1)) )
+
+  #first term is just counting how many people experience the terminal event without the nonterminal in each interval
+  temp_val <- tapply((1-delta1)*delta2,cut_cats2,sum)
+  if(length(temp_val) != num_int[2]){stop("It looks like you do not observe events in every interval of the second transition")}
+  score_phi2 <- temp_val - as.vector( t(Lambda02Phi) %*% (commonVec * exp(h + eta2)) )
+
+  #first term is just counting how many people experience the terminal event after the nonterminal in each interval
+  temp_val <- tapply((delta1)*delta2,cut_cats3,sum)
+  if(length(temp_val) != num_int[3]){stop("It looks like you do not observe events in every interval of the third transition")}
+  score_phi3 <- temp_val - as.vector(  t(Lambda03Phi) %*% (commonVec * exp(h + eta3)) )
+
+  #h (what ina calls u1)
+  score_h <- sum(exp(h)*(delta1 * delta2/(1+exp(h)) + log(1+exp(h) * AVec)/exp(2*h) - commonVec * AVec))
+
+  #beta1 (what ina calls u2)
+  if(nP1 == 0){ score_beta1 <- NULL } else{
+    score_beta1 <-  t(Xmat1) %*% (delta1 - commonVec * Lambda01 * exp(h + eta1))
+  }
+
+  #beta2 (what ina calls u3)
+  if(nP2 == 0){ score_beta2 <- NULL } else{
+    score_beta2 <- t(Xmat2) %*% ( (1-delta1) * delta2 - commonVec * Lambda02 * exp(h + eta2))
+  }
+
+  #beta3 (what ina calls u4)
+  if(nP3 == 0){ score_beta3 <- NULL } else{
+    score_beta3 <-  t(Xmat3) %*% (delta1 * delta2 - commonVec * Lambda03 * exp(h + eta3))
+  }
+
+  return(-c(score_phi1,score_phi2,score_phi3,score_h,score_beta1,score_beta2,score_beta3))
+
+}
+
+# FUNCTION TO GET 'BASIS' MATRIX OF TIME ACCRUED IN EACH ARM ------------
+
+pw_cum_mat <- function(y, knots){
+  # browser()
+
+  if(knots[1] != 0){knots <- c(0,knots)}
+  knots_diff <- diff(knots)
+
+  #count of the number of intervals in each list
+  num_int <- c(length(knots))
+  n <- length(y)
+
+  #matrix with each row being an observation, and each column being an interval, capturing how much time is accrued in each interval
+  knots_mat <- matrix(c(knots_diff,0),nrow=n,ncol=num_int,byrow = TRUE)
+
+  #an n length vector of integers indicating which interval the observation is in
+  cut_cats <- findInterval(x = y, vec = knots)
+
+  #an n length vector capturing the residual time accrued in the final interval of each observation
+  y_last <- y-knots[cut_cats]
+
+  #a loop through each observation to finalize the matrix of time intervals
+  for(i in 1:n){
+    knots_mat[i,cut_cats[i]] <- y_last[i]
+    knots_mat[i,-c(1:cut_cats[i])] <- 0
+  }
+  return(knots_mat)
+}
+
---
+++ R/royston_parmar.R
@@ -0,0 +1,82 @@
+ns_d <- function (x, df = NULL, knots = NULL, intercept = FALSE, Boundary.knots = range(x))
+{
+
+  #draws from rstpm2 package nsxD function, extending the ns function from splines
+  #generate derivative basis of natural cubic spline function
+  #I purposefully write this function as the derivative of ns, for use with Royston parmar model
+
+  nx <- names(x)
+  x <- as.vector(x)
+  nax <- is.na(x)
+
+  #temporarilty set aside all NA values
+  if (nas <- any(nax))
+    x <- x[!nax]
+
+  #create a boolean flag for whether any of the given x values are outside of the boundaries
+  outside <- if (!missing(Boundary.knots)) {
+    Boundary.knots <- sort(Boundary.knots)
+    (ol <- x < Boundary.knots[1L]) | (or <- x > Boundary.knots[2L])
+  } else {
+    if (length(x) == 1L)
+      Boundary.knots <- x * c(7, 9)/8
+    FALSE
+  }
+
+  #if df is given instead of actual knot locations, fill in knots based on quantiles
+  if (!is.null(df) && is.null(knots)) {
+    nIknots <- df - 1L - intercept
+    if (nIknots < 0L) {
+      nIknots <- 0L
+      warning(gettextf("'df' was too small; have used %d",
+                       1L + intercept), domain = NA)
+    }
+    #notice this funny nesting, 'knots' inside is sequence of percentiles, and then reassigned as the quantiles of internal data
+    knots <- if (nIknots > 0L) {
+      knots <- seq.int(from = 0, to = 1, length.out = nIknots +
+                         2L)[-c(1L, nIknots + 2L)]
+      quantile(x[!outside], knots)
+    }
+  } else{ nIknots <- length(knots) }
+  Aknots <- sort(c(rep(Boundary.knots, 4L), knots)) #this code essentially puts two copies of each boundary on the ends, forming intervals
+  if (any(outside)) {
+    basis <- array(0, c(length(x), nIknots + 4L))
+    if (any(ol)) {
+      k.pivot <- Boundary.knots[1L]
+      tt <- splines::splineDesign(Aknots, rep(k.pivot, sum(ol)), 4,
+                         1)
+      basis[ol, ] <- tt
+    }
+    if (any(or)) {
+      k.pivot <- Boundary.knots[2L]
+      tt <- splines::splineDesign(Aknots, rep(k.pivot, sum(or)), 4,
+                         1)
+      basis[or, ] <- tt
+    }
+    if (any(inside <- !outside))
+      basis[inside, ] <- splines::splineDesign(Aknots, x[inside],
+                                      4,derivs = 1)
+  } else{ basis <- splines::splineDesign(Aknots, x, ord = 4L,derivs = 1) }
+  const <- splines::splineDesign(Aknots, Boundary.knots, ord = 4L, derivs = c(2L,
+                                                                     2L))
+  if (!intercept) {
+    const <- const[, -1, drop = FALSE]
+    basis <- basis[, -1, drop = FALSE]
+  }
+  qr.const <- qr(t(const))
+  basis <- as.matrix((t(qr.qty(qr.const, t(basis))))[, -(1L:2L),
+                                                     drop = FALSE])#uses QR decomp to orthogonalize the basis
+  n.col <- ncol(basis)
+
+  if (nas) {
+    nmat <- matrix(NA, length(nax), n.col)
+    nmat[!nax, ] <- basis
+    basis <- nmat
+  }
+  dimnames(basis) <- list(nx, 1L:n.col)
+  a <- list(degree = 3L, knots = if (is.null(knots)) numeric() else knots,
+            Boundary.knots = Boundary.knots, intercept = intercept)
+  attributes(basis) <- c(attributes(basis), a)
+  class(basis) <- c("ns_d", "basis", "matrix")
+  basis
+}
